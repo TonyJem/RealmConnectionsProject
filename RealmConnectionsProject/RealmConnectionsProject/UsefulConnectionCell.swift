01c073ea0823d48e343f136d512c86bb65d2a93f
TonyJem <a.jemanov@gmail.com>
2021-04-04 08:03:25 +0300

    Add UsefulConnectionCell file

---
+++ RealmConnectionsProject/RealmConnectionsProject/UsefulConnectionCell.swift
@@ -0,0 +1,14 @@
+import UIKit
+ 
+class UsefulConnectionCell: UITableViewCell {
+ 
+  @IBOutlet weak var emailLabel: UILabel!
+  @IBOutlet weak var descriptionLabel: UILabel!
+  @IBOutlet weak var ratingLabel: UILabel!
+ 
+  func configureCell(with model: UsefulConnectionModel){
+    emailLabel.text = model.emailLabel
+    descriptionLabel.text = model.descriptionLabel
+    ratingLabel.text = model.ratingString()
+  }
+}
