6a7913b8a861534e1242a1af079e4468d277f575
TonyJem <a.jemanov@gmail.com>
2021-04-04 08:04:28 +0300

    Add UsefulConnectionModel file

---
+++ RealmConnectionsProject/RealmConnectionsProject/UsefulConnectionModel.swift
@@ -0,0 +1,19 @@
+import RealmSwift
+ 
+ class UsefulConnectionModel: Object  {
+  @objc dynamic var emailLabel: String? = nil
+  @objc dynamic var descriptionLabel: String? = nil
+  let ratingLabel = RealmOptional<Int>()
+ 
+  convenience init(email: String?, description: String?, rating: Int?) {
+    self.init()
+    emailLabel = email
+    descriptionLabel = description
+    ratingLabel.value = rating
+  }
+ 
+  func ratingString() -> String? {
+    guard let value = ratingLabel.value else { return nil }
+    return String(value)
+  }
+}
